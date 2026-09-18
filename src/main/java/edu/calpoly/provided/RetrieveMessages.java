adf10cef8a7ee7ee007d9a8760d0e789828c00f9
ReeledWarrior14 <anay.nagar@outlook.com>
2026-09-18 13:00:49 -0700

    Implement sending retrieved messages with Broker

--- src/main/java/edu/calpoly/provided/RetrieveMessages.java
+++ src/main/java/edu/calpoly/provided/RetrieveMessages.java
@@ -21,10 +21,13 @@ public class RetrieveMessages {
             return;
         }
 
+        Broker broker = new Broker("localhost", PORT);
+
         System.out.println("Retrieved " + messages.size() + " messages from " + DATA_FILE.toAbsolutePath());
 
         for (String message : messages) {
             System.out.println("Sending message: " + message);
+            broker.send(message);
         }
     }
 
